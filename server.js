a53b38296ff5c74ef96dc52a0e6e67405332f993
Andrew Yang <ayang24@email.unc.edu>
2022-12-02 00:56:08 -0500

    final commit

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+//import dependencies
 import { roll } from './lib/roll.js';
 import express from 'express';
 import minimist from 'minimist';
 
+//setup
 const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
